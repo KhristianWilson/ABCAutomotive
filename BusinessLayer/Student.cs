a6f90a6f83d379ebe34aa4041c3caab08e091c6a
Khris <khristiandlw@gmail.com>
2017-10-27 21:35:55 -0300

    Added DataAccess Layer Started BusinessLayer

---
+++ BusinessLayer/Student.cs
@@ -0,0 +1,63 @@
+﻿using ABCAutomotive.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABCAutomotive.BusinessLayer
+{
+    public class Student
+    {
+
+        #region Constructor
+
+        internal Student()
+        {
+
+        }
+
+        #endregion
+
+        #region Local fields and Flags
+
+        internal int _studentid;
+        internal string _firstName;
+        internal string _lastName;
+        internal string _address;
+        internal string _postalcode;
+        internal string _city;
+        internal string _phone;
+        internal double _balancedue;
+        internal ProgramType _programType;
+        internal StudentStatus _status;
+        internal DateTime _startDate;
+        internal DateTime _endDate;
+
+        internal Boolean _trusted = false;
+
+        #endregion
+
+        #region Local Properties
+
+        internal bool IsClean
+        {
+            get
+            {
+                if (_studentid == 0 || string.IsNullOrEmpty(_firstName) || string.IsNullOrEmpty(_lastName) || string.IsNullOrEmpty(_address) || string.IsNullOrEmpty(_postalcode) || string.IsNullOrEmpty(_phone) || string.IsNullOrEmpty(_city))
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+    }
+}
---
+++ DataAccessLayer/DataAccess.cs
@@ -0,0 +1,84 @@
+﻿using ABCAutomotive.Types;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ABCAutomotive.DAL
+{
+    public sealed class DataAccess
+    {
+        private DataAccess() { }
+
+        #region Database Access Methods
+
+        public static DataTable GetDataTable(string SQLStatement, List<ParmStruct> parmlist = null)
+        {
+            SqlCommand cmd = CreateCommandObject(SQLStatement, parmlist);
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            return dt;
+        }
+
+        public static DataSet GetDataSet(string SQLStatement, List<ParmStruct> parmlist = null)
+        {
+            SqlCommand cmd = CreateCommandObject(SQLStatement, parmlist);
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            da.SelectCommand = cmd;
+            da.Fill(ds);
+            return ds;
+        }
+
+        public static int SendData(string SQLStatement, List<ParmStruct> parmlist = null)
+        {
+            SqlCommand cmd = CreateCommandObject(SQLStatement, parmlist);
+            using (cmd.Connection)
+            {
+                cmd.Connection.Open();
+                UnloadParms(cmd, parmlist);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static SqlCommand CreateCommandObject(string commond, List<ParmStruct> parms)
+        {
+            SqlConnection cnn = new SqlConnection(Properties.Settings.Default.CnnString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = commond;
+            cmd.Connection = cnn;
+            cmd.CommandType = CommandType.StoredProcedure;
+            if (parms != null)
+            {
+                foreach (ParmStruct x in parms)
+                {
+                    cmd.Parameters.Add(new SqlParameter(x.parmName, x.datatype, x.size));
+                    cmd.Parameters[x.parmName].Value = x.parmValue;
+                    cmd.Parameters[x.parmName].Direction = x.direction;
+                }
+            }
+            return cmd;
+        }
+
+        private static void UnloadParms(SqlCommand cmd, List<ParmStruct> parms)
+        {
+            if (parms != null)
+            {
+                for (int i = 0; i <= parms.Count - 1; i++)
+                {
+                    ParmStruct x = parms[i];
+                    x.parmValue = cmd.Parameters[i].Value;
+                    parms[i] = x;
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}
